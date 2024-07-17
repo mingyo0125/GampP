758cf7550c4d9f0b2207913ed18d1e54a0162b2a
mingyo0125 <wjdalsry125@gmail.com>
2024-07-18 00:08:35 +0900

    Add Player Follow Camera synchronization

--- Assets/01.Scripts/Objects/Player/PlayerNetWork.cs
+++ Assets/01.Scripts/Objects/Player/PlayerNetWork.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -8,12 +9,30 @@ public class PlayerNetWork : NetworkBehaviour
     #region PlayerComponents
 
     private PlayerMovement _playerMovement;
+    private CinemachineVirtualCamera _virtualCamera;
+    private AudioListener _audioListener;
 
     #endregion
 
     private void Awake()
     {
         _playerMovement = GetComponent<PlayerMovement>();
+        _virtualCamera = transform.Find("PlayerFollowCam").GetComponent<CinemachineVirtualCamera>();
+        _audioListener = transform.Find("Camera").GetComponent<AudioListener>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if(IsOwner)
+        {
+            _virtualCamera.Priority = 1;
+            _audioListener.enabled = true;
+        }
+        else
+        {
+            _virtualCamera.Priority = 0;
+            _audioListener.enabled = false;
+        }
     }
 
     private void Update()
