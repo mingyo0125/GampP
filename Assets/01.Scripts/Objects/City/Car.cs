f4b1f54b1b32539f95153b26e864b5b5f577ecd8
mingyo0125 <wjdalsry125@gmail.com>
2024-08-30 12:51:52 +0900

    Add Spawn&Destroy Car

---
+++ Assets/01.Scripts/Objects/City/Car.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Car : NetworkBehaviour
+{
+    CharacterController _characterController;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveVec = transform.forward * Time.fixedDeltaTime * 20;
+
+        _characterController.Move(moveVec);
+    }
+}
---
+++ Assets/01.Scripts/Objects/City/CarSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _carPrefab;
+
+    private void SpawnCar()
+    {
+        Instantiate(_carPrefab, transform.position, transform.rotation);
+    }
+
+    [ClientRpc]
+    private void SpawnCarClientRpc()
+    {
+        SpawnCar();
+    }
+
+    private void Update()
+    {
+        //if (!LobbyManager.Instance.ClientInfo.IsServer) { return; }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnCarClientRpc();
+        }
+    }
+}
